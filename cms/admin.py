f25633f29037e6f4155c8dce4c35e12b00b68642
Andrey Shishkov <shishkoffandrey@yandex.ru>
2023-04-02 20:48:40 +0300

    Add left parts

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import MenuItem
+
 
 # Register your models here.
+class MenuItemMPTTModelAdmin(MPTTModelAdmin):
+    mptt_level_indent = 20
+
+
+admin.site.register(MenuItem, MenuItemMPTTModelAdmin)
--- cms/templatetags/common_tags.py
+++ cms/templatetags/common_tags.py
@@ -1,11 +1,11 @@
 from django import template
-from cms.models import Menu
+from cms.models import MenuItem
 register = template.Library()
 
 
-@register.inclusion_tag('menu.html', takes_context=True)
-def show_root_menu(context):
-    menu_items = Menu.objects.all()
+@register.inclusion_tag('templatetags/menu.html', takes_context=True)
+def show_menu(context):
+    menu_items = MenuItem.objects.filter(level=1)
     return {
-        'menu_items': menu_items,
+        "menu_items": menu_items,
     }
